6704e7b36696f2e1df5b51479fecb18515f9adfb
sun98117 <sky98117@gmail.com>
2017-04-17 23:14:04 +0900

    Add files via upload

---
+++ 20170410hw.py
@@ -0,0 +1,9 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn = turtle.Screen()
+>>> t1 = turtle.Turtle()
+>>> wn.addshape('C:\\Users\\DO\\Documents\\카카오톡 받은 파일\\KakaoTalk_20170417_111023756.gif')
+>>> t1.shape('C:\\Users\\DO\\Documents\\카카오톡 받은 파일\\KakaoTalk_20170417_111023756.gif')
+>>> t1.fd(200)
+>>> 
