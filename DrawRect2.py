b46338873dec54c287bb42f5847120cb00c5c093
sun98117 <sky98117@gmail.com>
2017-05-01 16:38:51 +0900

    Add files via upload

---
+++ DrawRect2.py
@@ -0,0 +1,13 @@
+﻿import turtle
+wn = turtle.Screen()
+t1 = turtle.Turtle()
+t1.speed(2)
+t1.forward(100)
+t1.right(90)
+t1.forward(100)
+t1.right(90)
+t1.forward(100)
+t1.right(90)
+t1.forward(100)
+
+wn.exitonclick()
